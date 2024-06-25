d90677f4f0d93dcccca48b7cddcc84952dfd5fa1
0xb39 <giiguu.giigu@gmail.com>
2024-06-25 13:01:58 +0800

    connected map with api

--- dashboard-frontend/src/data/map.jsx
+++ dashboard-frontend/src/data/map.jsx
@@ -1,31 +1,42 @@
 /* eslint-disable no-unused-vars */
-import { Icon, marker } from "leaflet";
-import React, {useState} from "react";
-import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents  } from "react-leaflet";
-import MarkerClusterGroup from 'react-leaflet-cluster'
+import { Icon } from "leaflet";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import MarkerClusterGroup from 'react-leaflet-cluster';
 
 export default function Map() {
   const [markers, setMarkers] = useState([]);
-  const markerUrl = '../../public/marker.png'
+  const [data, setData] = useState([]);
+  const markerUrl = 'marker.png';
 
   const customIcon = new Icon({
     iconUrl: markerUrl,
-    iconSize: [38,38] 
-  })
+    iconSize: [38, 38]
+  });
 
-  function AddMarkerOnClick() {
-    useMapEvents({
-      click(e) {
-        const { lat, lng } = e.latlng;
-        setMarkers((prevMarkers) => [...prevMarkers, { lat, lng }]);
-      },
-    });
-    return null;
-  }
-
+  useEffect(() => {
+    axios.post('http://localhost:3001/api/devices')
+      .then(response => {
+        
+        const devices = response.data.body;
+        const newMarkers = devices
+          .filter(device => device.device_user_geolocation_latitude != null && device.device_user_geolocation_longitude != null)
+          .map(device => ({
+            lat: device.device_user_geolocation_latitude,
+            lng: device.device_user_geolocation_longitude,
+            user: device.device_user_id,
+          }));
+        setMarkers(newMarkers);
+        setData(devices);
+      })
+      .catch(error => {
+        console.error('Error', error);
+      });
+  }, []);
 
   return (
-    <MapContainer center={[47.92262, 106.92618]} zoom={20} scrollWheelZoom={true} style={{ height: "100vh", width: "100%" }}>
+    <MapContainer center={[47.92262, 106.92618]} zoom={14} scrollWheelZoom={true} style={{ height: "100vh", width: "100%" }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -34,12 +45,13 @@ export default function Map() {
         {markers.map((marker, idx) => (
           <Marker key={idx} position={[marker.lat, marker.lng]} icon={customIcon}>
             <Popup>
-              {marker.lat}, {marker.lng}
+              Хэрэглэгч: {marker.user}
+              <br />
+              Өргөрөг: {marker.lat}, Уртраг: {marker.lng}
             </Popup>
           </Marker>
         ))}
       </MarkerClusterGroup>
-      <AddMarkerOnClick />
     </MapContainer>
   );
 }
