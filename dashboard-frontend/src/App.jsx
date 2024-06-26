029fd471d0f7b5c58b440c1b53aa61fd97ea2f58
0xb39 <giiguu.giigu@gmail.com>
2024-06-26 16:42:45 +0800

    added notfoundpage

--- dashboard-frontend/src/App.jsx
+++ dashboard-frontend/src/App.jsx
@@ -7,13 +7,14 @@ import { Devices, Area, Bar, Line, Pie, Dashboard, Home } from './pages'
 import {useStateContext} from './contexts/ContextProvider'
 import './App.css';
 import Map from './data/map';
+import NotFoundPage from './components/NotFoundPage';
 const App = () => {
   const {activeMenu} = useStateContext();
   return (
     <BrowserRouter>
       <div className='flex relative dark:bg-main-dark-bg z-0'>
         {activeMenu ? (
-          <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-blue-950'>
+          <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-blue-800'>
             <Sidebar/>
           </div>
         ) : (
@@ -22,7 +23,7 @@ const App = () => {
           </div>
         )}
         <div className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full dark:text-white ${activeMenu ? 'md:ml-72' :'flex-2'}`}>
-          <div className='fixed md:static dark:bg-main-dark-bg navbar w-full bg-blue-950 text-white'>
+          <div className='fixed md:static dark:bg-main-dark-bg navbar w-full bg-blue-800 text-white'>
             <Navbar />
           </div>
         
@@ -34,6 +35,7 @@ const App = () => {
             {/* Pages */}
             <Route path='/devices' element={<Devices />}/>  
             <Route path='/map' element={<Map />}/> 
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
         </div>
